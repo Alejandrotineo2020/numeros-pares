6d6c4d0707b06d7618253fa699e404e8e742d228
Alejandro M. Tineo Moreta <115231713+Alejandrotineo2020@users.noreply.github.com>
2026-03-16 20:31:28 -0400

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace numero8
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace numero8
+{
+    public partial class MainPage : ContentPage
+    {
+       public List<int> MisNumeros { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            GenerarNumeros(100);
+         
+
+
+        }
+
+      
+
+
+        private void GenerarNumeros (int limite)
+        {
+            List<int> listaNumeros = new List<int>();
+
+            int i = 2;
+
+            while (i <= limite)
+            {
+                listaNumeros.Add(i);
+
+                i += 2;
+            }
+
+
+            NumerosListView.ItemsSource = listaNumeros;
+        }
+
+
+
+    }
+}
